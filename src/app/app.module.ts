f55ab98dfaca9750e2f4c0cb43a1c47c044a0ed6
mcwilliamska <mcwilliamska2016@gmail.com>
2020-09-23 15:05:35 -0400

    Added summary for one stock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { StocksService } from './services/stocks.service';
+import { SummaryComponent } from './components/summary/summary.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ StocksService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpBackend, HttpClient } from '@angular/common/http';
+
+let stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+let service: string = 'https://angular2-in-action-api.herokuapp.com/';
+
+export interface StockInterface {
+  symbol: string;
+  lastTradePriceOnly: number;
+  change: number;
+  changeInPercent: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class StocksService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get() {
+    return stocks.slice();
+  }
+
+  add(stock) {
+    stocks.push(stock);
+    return this.get();
+  }
+
+  remove(stock): any {
+    stocks.splice(stocks.indexOf(stock), 1);
+    return this.get();
+  }
+
+  load(symbols): any {
+    if (symbols) {
+      return this.http.get<Array<StockInterface>>(service + 'stocks/snapshot?symbols=' + symbols.join());
+    }
+  }
 }
